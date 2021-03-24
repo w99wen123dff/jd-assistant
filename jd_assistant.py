46f2213e1e98ba039a67b3a6ec2af93851257739
w99wen <w99wen@gmail.com>
2021-03-25 02:27:48 +0800

    ss

--- jd_assistant.py
+++ jd_assistant.py
@@ -636,6 +636,7 @@ class Assistant(object):
         url = 'https://cart.jd.com/gate.action'
         headers = {
             'User-Agent': self.user_agent,
+            'referer': 'https://item.jd.com/',
         }
 
         for sku_id, count in parse_sku_id(sku_ids=sku_ids).items():
@@ -655,6 +656,31 @@ class Assistant(object):
                 logger.info('%s x %s 已成功加入购物车', sku_id, count)
             else:
                 logger.error('%s 添加到购物车失败', sku_id)
+    @check_login
+    def add_item_to_cart_2(self, sku_ids):
+        """添加商品到购物车
+
+        重要：
+        1.商品添加到购物车后将会自动被勾选✓中。
+        2.在提交订单时会对勾选的商品进行结算。
+        3.部分商品（如预售、下架等）无法添加到购物车
+
+        京东购物车可容纳的最大商品种数约为118-120种，超过数量会加入购物车失败。
+
+        :param sku_ids: 商品id，格式："123" 或 "123,456" 或 "123:1,456:2"。若不配置数量，默认为1个。
+        :return:
+        """
+        
+
+        for sku_id, count in parse_sku_id(sku_ids=sku_ids).items():
+            data = {}
+            url = 'https://cart.jd.com/tproduct?' + 'pid={0}&rid={1}'.format(sku_id, random.random())
+            resp = self.sess.post(url=url, data=data)
+            result = True
+            if not response_status(resp):
+                logger.info('add_item_to_cart_2 完成')
+            else: 
+                logger.info('add_item_to_cart_2 错误')
 
     @check_login
     def clear_cart(self):
@@ -791,7 +817,8 @@ class Assistant(object):
             )
         else:
             logger.info('%s 不在购物车中，开始加入购物车，数量 %s', sku_id, count)
-            return self.add_item_to_cart(sku_ids={sku_id: count})
+            self.add_item_to_cart(sku_ids={sku_id: count}) 
+            return self.add_item_to_cart_2(sku_ids={sku_id: count})
 
     @check_login
     def get_checkout_page_detail(self):
@@ -805,6 +832,7 @@ class Assistant(object):
         # url = 'https://cart.jd.com/gotoOrder.action'
         payload = {
             'rid': str(int(time.time() * 1000)),
+            'referer':'https://cart.jd.com/',
         }
         try:
             resp = self.sess.get(url=url, params=payload)
@@ -899,7 +927,7 @@ class Assistant(object):
 
         :return: True/False 订单提交结果
         """
-        url = 'https://trade.jd.com/shopping/order/submitOrder.action'
+        url = 'https://trade.jd.com/shopping/order/submitOrder.action?&presaleStockSign=1'
         # js function of submit order is included in https://trade.jd.com/shopping/misc/js/order.js?r=2018070403091
 
         data = {
@@ -907,15 +935,13 @@ class Assistant(object):
             'vendorRemarks': '[]',
             'submitOrderParam.sopNotPutInvoice': 'false',
             'submitOrderParam.trackID': 'TestTrackId',
+            'presaleStockSign': '1',
             'submitOrderParam.ignorePriceChange': '0',
             'submitOrderParam.btSupport': '0',
-            'riskControl': self.risk_control,
-            'submitOrderParam.isBestCoupon': 1,
-            'submitOrderParam.jxj': 1,
-            'submitOrderParam.trackId': self.track_id,  # Todo: need to get trackId
+            'submitOrderParam.isBestCoupon': '1',
             'submitOrderParam.eid': self.eid,
             'submitOrderParam.fp': self.fp,
-            'submitOrderParam.needCheck': 1,
+            'submitOrderParam.jxj': '1',
         }
 
         # add payment password when necessary
@@ -1370,7 +1396,7 @@ class Assistant(object):
             logger.info('执行结束，提交订单失败！')
 
     @check_login
-    def buy_item_in_stock(self, sku_ids, area, wait_all=False, stock_interval=3, submit_retry=3, submit_interval=0.3, timerMode=False):
+    def buy_item_in_stock(self, sku_ids, area, wait_all=False, stock_interval=3, submit_retry=3, submit_interval=5, timerMode=False):
         """根据库存自动下单商品
         :param sku_ids: 商品id。可以设置多个商品，也可以带数量，如：'1234' 或 '1234,5678' 或 '1234:2' 或 '1234:2,5678:3'
         :param area: 地区id
@@ -1389,31 +1415,31 @@ class Assistant(object):
 
         if not wait_all:
             logger.info('下单模式：%s 任一商品有货并且未下架均会尝试下单', items_list)
-            while True:
-                for (sku_id, count) in items_dict.items():
-                    if not self.if_item_can_be_ordered(sku_ids={sku_id: count}, area=area_id):
-                        logger.info('%s 不满足下单条件，%ss后进行下一次查询', sku_id, stock_interval)
-                    else:
-                        logger.info('%s 满足下单条件，开始执行', sku_id)
-                        self._cancel_select_all_cart_item()
-                        self._add_or_change_cart_item(self.get_cart_detail(), sku_id, count)
-                        if self.submit_order_with_retry(submit_retry, submit_interval + random.randint(0, 300) / 1000):
-                            return
-
-                    time.sleep(stock_interval)
-        else:
-            logger.info('下单模式：%s 所有都商品同时有货并且未下架才会尝试下单', items_list)
-            while True:
-                if not self.if_item_can_be_ordered(sku_ids=sku_ids, area=area_id):
-                    logger.info('%s 不满足下单条件，%ss后进行下一次查询', items_list, stock_interval + random.randint(0, 300) / 1000)
+            # while True:
+            for (sku_id, count) in items_dict.items():
+                if not self.if_item_can_be_ordered(sku_ids={sku_id: count}, area=area_id):
+                    logger.info('%s 不满足下单条件，%ss后进行下一次查询', sku_id, stock_interval)
                 else:
-                    logger.info('%s 满足下单条件，开始执行', items_list)
+                    logger.info('%s 满足下单条件，开始执行', sku_id)
                     self._cancel_select_all_cart_item()
-                    shopping_cart = self.get_cart_detail()
-                    for (sku_id, count) in items_dict.items():
-                        self._add_or_change_cart_item(shopping_cart, sku_id, count)
-
-                    if self.submit_order_with_retry(submit_retry, submit_interval):
+                    self._add_or_change_cart_item(self.get_cart_detail(), sku_id, count)
+                    if self.submit_order_with_retry(submit_retry, submit_interval + random.randint(0, 300) / 1000):
                         return
 
-                time.sleep(stock_interval)
+                    # time.sleep(stock_interval)
+        else:
+            logger.info('下单模式：%s 所有都商品同时有货并且未下架才会尝试下单', items_list)
+            # while True:
+            if not self.if_item_can_be_ordered(sku_ids=sku_ids, area=area_id):
+                logger.info('%s 不满足下单条件，%ss后进行下一次查询', items_list, stock_interval + random.randint(0, 1000) / 1000)
+            else:
+                logger.info('%s 满足下单条件，开始执行', items_list)
+                self._cancel_select_all_cart_item()
+                shopping_cart = self.get_cart_detail()
+                for (sku_id, count) in items_dict.items():
+                    self._add_or_change_cart_item(shopping_cart, sku_id, count)
+
+                if self.submit_order_with_retry(submit_retry, submit_interval + random.randint(0, 1000) / 1000):
+                    return
+
+                # time.sleep(stock_interval)
